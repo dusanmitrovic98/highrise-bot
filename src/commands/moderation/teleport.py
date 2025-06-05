8d3a192b674abb541ff6376396a4d9dfaea44da9
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2025-06-05 19:54:38 +0200

    update

--- src/commands/moderation/teleport.py
+++ src/commands/moderation/teleport.py
@@ -28,43 +28,11 @@ class Command(CommandBase):
         else:
             target_user_id = user.id
             pos_args = args
-        def normalize_facing(facing_str, default):
-            facing_map = {
-                'frontright': 'FrontRight',
-                'frontleft': 'FrontLeft',
-                'backright': 'BackRight',
-                'backleft': 'BackLeft',
-                'front': 'FrontRight',
-                'back': 'BackRight',
-                'left': 'FrontLeft',
-                'right': 'FrontRight',
-            }
-            key = facing_str.replace(' ', '').lower()
-            return facing_map.get(key, default)
 
-        if len(pos_args) == 3 or len(pos_args) == 4:
+        if len(pos_args) == 3:
             try:
                 x, y, z = float(pos_args[0]), float(pos_args[1]), float(pos_args[2])
-                if len(pos_args) == 4:
-                    facing_raw = pos_args[3]
-                    # Get user's current facing if not valid
-                    user_pos = None
-                    for content in response.content:
-                        if content[0].id == target_user_id:
-                            if len(content) > 1:
-                                user_pos = content[1]
-                            break
-                    default_facing = getattr(user_pos, 'facing', 'FrontRight') if user_pos else 'FrontRight'
-                    facing = normalize_facing(facing_raw, default_facing)
-                else:
-                    user_pos = None
-                    for content in response.content:
-                        if content[0].id == target_user_id:
-                            if len(content) > 1:
-                                user_pos = content[1]
-                            break
-                    facing = getattr(user_pos, 'facing', 'FrontRight') if user_pos else 'FrontRight'
-                dest = Position(x, y, z, facing)
+                dest = Position(x, y, z)
             except ValueError:
                 await self.bot.highrise.send_whisper(user.id, "Invalid coordinates")
                 return
@@ -73,10 +41,7 @@ class Command(CommandBase):
             locations_dict = get_section('locations')
             loc = locations_dict.get(position_name)
             if loc and all(k in loc for k in ("x", "y", "z")):
-                facing = loc.get("facing", "FrontRight")
-                facing = normalize_facing(facing, "FrontRight")
-                dest = Position(loc["x"], loc["y"], loc["z"], facing)
-                await self.bot.highrise.chat(f"Teleporting to {dest}!")
+                dest = Position(loc["x"], loc["y"], loc["z"])
             else:
                 await self.bot.highrise.send_whisper(user.id, f"{position_name} is not a valid position in this room.")
                 return
